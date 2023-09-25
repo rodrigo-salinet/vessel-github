6fd030a705db575ab210e4eae5d89f06c0565701
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-09-25 00:23:12 -0300

    ✨ add dbx configurations

--- libs/config.sh
+++ libs/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # versão
-VER="2.0.0"
+VER="3.0.0"
 
 # caminho (path) absoluto do vessel
 V_PATH=$(pwd)
@@ -24,11 +24,20 @@ V_M2_PATH=${V_PATH}/magento2
 # caminho (path) absoluto do arquivo .env do magento2
 V_M2_ENV_FILE=${V_M2_PATH}/.env
 
+# caminho (path) absoluto do devilbox do vessel
+V_M3_PATH=${V_PATH}/devilbox-2.3
+
+# caminho (path) absoluto do arquivo .env do devilbox
+V_M3_ENV_FILE=${V_M3_PATH}/.env
+
 # caminho (path) absoluto das lojas m1 do vessel
 V_M1_WWW_PATH=${V_M1_PATH}/data/www
 
 # caminho (path) absoluto das lojas m2 do vessel
 V_M2_WWW_PATH=${V_M2_PATH}/data/www
+
+# caminho (path) absoluto das lojas m3 do vessel
+V_M3_WWW_PATH=${V_M3_PATH}/data/www
 
 # Github URL's
 GITHUB_BASE_URL=https://github.com/api/v4/
@@ -52,6 +61,7 @@ source ${V_LIBS_PATH}/menu.sh
 source ${V_LIBS_PATH}/common.sh
 source ${V_LIBS_PATH}/magento1.sh
 source ${V_LIBS_PATH}/magento2.sh
+source ${V_LIBS_PATH}/dbx.sh
 
 # caminho (path) do container/lojas magento atual
 M_PATH=$V_M1_PATH
@@ -61,6 +71,12 @@ if [ $MAGENTO == 2 ]; then
     M_PATH=$V_M2_PATH
     M_WWW_PATH=$V_M2_WWW_PATH
     M_ENV_FILE=$V_M2_ENV_FILE
+fi
+
+if [ $MAGENTO == 3 ]; then
+    M_PATH=$V_M3_PATH
+    M_WWW_PATH=$V_M3_WWW_PATH
+    M_ENV_FILE=$V_M3_ENV_FILE
 fi
 
 # Opções de execução do composer
@@ -81,12 +97,20 @@ M1_ADMIN_PASS='admin'
 M2_ADMIN_USER='local'
 M2_ADMIN_PASS='local123'
 
+M3_ADMIN_USER='local'
+M3_ADMIN_PASS='local123'
+
 LOCAL_ADMIN_USER=$M1_ADMIN_USER
 LOCAL_ADMIN_PASS=$M1_ADMIN_PASS
 
 if [ $MAGENTO == 2 ]; then
     LOCAL_ADMIN_USER=$M2_ADMIN_USER
     LOCAL_ADMIN_PASS=$M2_ADMIN_PASS
+fi
+
+if [ $MAGENTO == 3 ]; then
+    LOCAL_ADMIN_USER=$M3_ADMIN_USER
+    LOCAL_ADMIN_PASS=$M3_ADMIN_PASS
 fi
 
 # caminho executável do magento, da raiz de qualquer loja
